ff1a1e22e366cf5be6ae3df9fe8ca36554ca44b1
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-03-23 23:51:20 +0100

    added bill splitting function to project

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 print("Enter the number of friends joining (including you):")
-number_of_friends = int(input())
-if number_of_friends < 1:
+try:
+    number_of_friends = int(input())
+except:
     print("No one is joining for the party")
 else:
-    print("Enter the name of every friend (including you), each on a new line:")
-    friends = {}
-    for _ in range(number_of_friends):
-        friends[input()] = 0
-    print(friends)
+    if number_of_friends < 1:
+        print("No one is joining for the party")
+    else:
+        print("Enter the name of every friend (including you), each on a new line:")
+        friends = {}
+        for _ in range(number_of_friends):
+            friends[input()] = 0
+        print("Enter the total bill value:")
+        bill = float(input())
+        for i in friends:
+            friends[i] = round(bill / number_of_friends, 2)
+        print(friends)
